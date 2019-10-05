efe4397a41095886ccb1fd30b36fdd72f7cef2e9
HaishengZhangR3 <haisheng.zhang@r3.com>
2019-10-05 15:38:39 +0800

    add integration test: happy path for create-reply-prpose close-agree close-close.
    but:
    3. simultaneously trigger of reply or add/remove or close not tested,
    4. performance not yet,

--- examples/integrationTest/src/integrationTest/kotlin/com/r3/corda/lib/chat/examples/chatTest/IntegrationTest.kt
+++ examples/integrationTest/src/integrationTest/kotlin/com/r3/corda/lib/chat/examples/chatTest/IntegrationTest.kt
@@ -1,8 +1,8 @@
 package com.r3.corda.lib.accounts.examples.tokensTest
 
 import com.r3.corda.lib.chat.contracts.states.ChatInfo
-import com.r3.corda.lib.chat.workflows.flows.CreateChatFlow
-import com.r3.corda.lib.chat.workflows.flows.ReplyChatFlow
+import com.r3.corda.lib.chat.contracts.states.CloseChatState
+import com.r3.corda.lib.chat.workflows.flows.*
 import net.corda.core.identity.CordaX500Name
 import net.corda.core.internal.concurrent.transpose
 import net.corda.core.messaging.startFlow
@@ -14,6 +14,7 @@ import net.corda.testing.driver.NodeHandle
 import net.corda.testing.driver.NodeParameters
 import net.corda.testing.driver.driver
 import net.corda.testing.node.TestCordapp
+import org.junit.Assert
 import org.junit.Test
 
 class IntegrationTest {
@@ -55,11 +56,11 @@ class IntegrationTest {
     @Test
     fun `basic chat flow test`() {
         driver(driverParameters) {
+            log.warn("***** Chat integration test starting ....... *****")
             val (A, B, I) = nodeParams.map { params -> startNode(params) }.transpose().getOrThrow()
-            log.info("All nodes started up.")
+            log.warn("***** All nodes started up *****")
 
-            log.info("Creating chat on node A.")
-
+            log.warn("***** Creating chat on node A *****")
             val createChatOnA = listOf(
                     A.rpc.startFlow(
                             ::CreateChatFlow,
@@ -71,9 +72,9 @@ class IntegrationTest {
             ).transpose().getOrThrow()
 
             val chatOnA = createChatOnA.single().state.data
-            log.info("The chat ID created on A is: ${chatOnA.linearId}")
+            log.warn("***** The chat created on A is: ${chatOnA.linearId} *****")
 
-
+            log.warn("***** Reply chat from B *****")
             val replyChatFromB = listOf(
                     B.rpc.startFlow(
                             ::ReplyChatFlow,
@@ -84,8 +85,48 @@ class IntegrationTest {
             ).transpose().getOrThrow()
 
             val chatOnB = replyChatFromB.single().coreTransaction.outputStates.single() as ChatInfo
-            log.info("The chat ID replied from B is: ${chatOnB.linearId}")
+            log.warn("***** The chat replied from B: ${chatOnB.linearId} *****")
+            log.warn("***** Reply chat from B *****")
 
+            log.warn("***** Propose close from B *****")
+            val proposeFromB = listOf(
+                    B.rpc.startFlow(
+                            ::CloseChatProposeFlow,
+                            chatOnA.linearId
+                    ).returnValue
+            ).transpose().getOrThrow()
+
+            log.warn("***** Close proposal agreed from A *****")
+            val agreeFromA = listOf(
+                    A.rpc.startFlow(
+                            ::CloseChatAgreeFlow,
+                            chatOnA.linearId
+                    ).returnValue
+            ).transpose().getOrThrow()
+
+            log.warn("***** Do final close from B *****")
+            val doCloseFromA = listOf(
+                    B.rpc.startFlow(
+                            ::CloseChatFlow,
+                            chatOnA.linearId,
+                            false
+                    ).returnValue
+            ).transpose().getOrThrow()
+
+            log.warn("***** Chat ${chatOnA.linearId} closed *****")
+
+            log.warn("**** Now let's check the closed chat *****")
+            val chatsInA = A.rpc.vaultQuery(ChatInfo::class.java).states
+            val chatsInB = B.rpc.vaultQuery(ChatInfo::class.java).states
+            Assert.assertTrue("Should not be any chat in A", chatsInA.isEmpty())
+            Assert.assertTrue("Should not be any chat in B", chatsInB.isEmpty())
+
+            val closeStateA = A.rpc.vaultQuery(CloseChatState::class.java).states
+            val closeStateB = B.rpc.vaultQuery(CloseChatState::class.java).states
+            Assert.assertTrue("Should not be close states in A", closeStateA.isEmpty())
+            Assert.assertTrue("Should not be close states in B", closeStateB.isEmpty())
+
+            log.warn("**** All passed, happy *****")
         }
     }
 
